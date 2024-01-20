3b071a9e6aa3bb57d92ced6b50f724b97bdaa067
Impressius <157175581+Impressius@users.noreply.github.com>
2024-01-20 14:42:09 +0700

    Update Celcius Convert.py
    Add Main code

--- Program Konversi Suhu/Celcius Convert.py
+++ Program Konversi Suhu/Celcius Convert.py
@@ -1,1 +1,15 @@
+# Program Konversi Suhu Sederhana dari Celcius ke Suhu lainnya
 
+celcius = float(input("\nMasukkan Suhu Celcius: "))
+
+# Reamur
+reamur = (4/5) * celcius
+print("\nSuhu dalam Reamur adalah:",reamur, "Reamur")
+
+# Fahrenheit
+fahrenheit = ((9/5) * celcius) + 32
+print("Suhu dalam fahrenheit adalah:",fahrenheit, "fahrenheit")
+
+# Kelvin
+kelvin = celcius + 273
+print("Suhu dalam Kelvin adalah:",kelvin, "Kelvin")
