ad7284ca38f197cc15def59b4c374177db6c2b34
Impressius <157175581+Impressius@users.noreply.github.com>
2024-01-21 10:54:18 +0700

    Update Celcius Convert.py
    Add Start & End of the Program line

--- Program Konversi Suhu/Celcius Convert.py
+++ Program Konversi Suhu/Celcius Convert.py
@@ -1,5 +1,6 @@
 # Program Konversi Suhu Sederhana dari Celcius ke Suhu lainnya
 
+print("\n" + 5*"=" + " Program Konversi Suhu Celcius " + "="*5)
 celcius = float(input("\nMasukkan Suhu Celcius: "))
 
 # Reamur
@@ -13,3 +14,4 @@ print("Suhu dalam fahrenheit adalah:",fahrenheit, "fahrenheit")
 # Kelvin
 kelvin = celcius + 273
 print("Suhu dalam Kelvin adalah:",kelvin, "Kelvin")
+print("\n" + 5*"=" + " Akhir dari Program " + "="*5)
